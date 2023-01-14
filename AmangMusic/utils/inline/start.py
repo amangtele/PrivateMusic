11592119f103d028146647996b81ac0c07b90581
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-14 23:20:24 +0700

    Update start.py

--- AmangMusic/utils/inline/start.py
+++ AmangMusic/utils/inline/start.py
@@ -102,10 +102,9 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             )
         ]
     )
-    if GITHUB_REPO and OWNER:
+    if GITHUB_REPO:
         buttons.append(
             [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
                 InlineKeyboardButton(
                     text=_["S_B_6"], url=f"{GITHUB_REPO}"
                 ),
@@ -120,12 +119,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                     ),
                 ]
             )
-        if OWNER:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_7"], user_id=OWNER
-                    ),
                 ]
             )
     buttons.append(
