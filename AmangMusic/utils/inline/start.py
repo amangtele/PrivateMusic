d843fb8dc0ad2e7027313de74832edd9f852e238
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-15 10:19:07 +0700

    Update start.py

--- AmangMusic/utils/inline/start.py
+++ AmangMusic/utils/inline/start.py
@@ -21,7 +21,7 @@ def start_pannel(_):
             InlineKeyboardButton(
                 text=_["S_B_5"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
+            ),
         ]
   buttons = [
         [
