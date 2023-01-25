ca270e7a5bdede96893737cfbcaa86d1e26c9306
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-25 19:18:21 +0700

    Create autoclear.py

---
+++ AmangMusic/plugins/sudo/autoclear.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from AmangMusic.misc import SUDOERS
+from AmangMusic import app
+
+
+
+
+@app.on_message(
+    filters.command("clear")
+    & SUDOERS
+    & ~filters.forwarded
+    & ~filters.via_bot
+  
+d = "downloads"
+c = "cache"
+shutil.rmtree(d)
+shutil.rmtree(c)
+os.mkdir(c)
+os.mkdir(d)
+await message.reply_text("yey")
