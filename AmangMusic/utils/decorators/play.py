307e8abb327fd453b6b8c09d91bcbf862f84baa6
ᴀᴍᴀɴɢ <122607345+amangtele@users.noreply.github.com>
2023-02-10 13:00:28 +0700

    Update play.py

--- AmangMusic/utils/decorators/play.py
+++ AmangMusic/utils/decorators/play.py
@@ -20,8 +20,6 @@ from AmangMusic.utils.database import (get_cmode, get_lang,
                                        is_served_private_chat)
 from AmangMusic.utils.database.memorydatabase import is_maintenance
 from AmangMusic.utils.inline.playlist import botplaylist_markup
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
-from config import MUST_JOIN
 
 def PlayWrapper(command):
     async def wrapper(client, message):
@@ -88,17 +86,6 @@ def PlayWrapper(command):
             return await message.reply_text(
                 _["general_4"], reply_markup=upl
             )
-         if MUST_JOIN:
-            try:
-                await app.get_chat_member(MUST_JOIN, message.from_user.id)
-            except UserNotParticipant:
-                sub = await app.export_chat_invite_link(MUST_JOIN)
-                kontol = InlineKeyboardMarkup(
-                    [
-                        [InlineKeyboardButton("MUST JOIN", url=sub)]
-                    ]
-                )
-                return await message.reply_text(_["force_sub"].format(message.from_user.mention), reply_markup=kontol)
 
         if message.command[0][0] == "c":
             chat_id = await get_cmode(message.chat.id)
