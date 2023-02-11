515ef425ccdf2bcfaf52940761d1de22f1a96a11
ᴀᴍᴀɴɢ <122607345+amangtele@users.noreply.github.com>
2023-02-11 19:49:56 +0700

    Update play.py

--- AmangMusic/utils/decorators/play.py
+++ AmangMusic/utils/decorators/play.py
@@ -88,7 +88,7 @@ def PlayWrapper(command):
             return await message.reply_text(
                 _["general_4"], reply_markup=upl
             )
-                   if MUST_JOIN:
+         if MUST_JOIN:
             try:
                 await app.get_chat_member(MUST_JOIN, message.from_user.id)
             except UserNotParticipant:
